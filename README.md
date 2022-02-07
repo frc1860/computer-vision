3ec7ef9bedd2b9dc3171b48942760e6910137666
Pedro Cleto <cl3t0.sh@gmail.com>
2022-02-07 00:18:20 -0300

    feat(setup): adding initial code and installing black, mypy, flake8, etc

---
+++ README.md
@@ -0,0 +1,17 @@
+# FRC1860
+Here you can find all 1860 RaspberryPi codes!
+
+## Codes
+
+### cameraCode.py
+
+It's the main code that runs on our RaspberryPi. He gets the camera videos and calculate distance, horizontal angle and launcher angle to help roboRIO with information!
+
+### calcDistanceParams.py
+
+This code uses minimum squares method to calculate the best parameters to our distance function.
+
+## Authors
+
+* **[Pedro Cleto](https://github.com/cl3t0)** - pedrocaf@ufmg.br
+* **[Breno Bertone](https://github.com/Bertonebreno)** - bertonebreno@gmail.com
---
+++ calcDistanceParams.py
@@ -0,0 +1,70 @@
+from math import atan
+from time import time
+
+import numpy as np
+from scipy.optimize import minimize, root_scalar
+
+D = [92, 119, 148, 171, 198, 238, 284]
+Y = [63, 192, 313, 357, 509, 540, 641]
+n = len(D)
+
+initialTime = time()
+
+lastPosition = n - 1
+midPosition = int(n / 2)
+firstPosition = 0
+
+
+def initialGuessFunction(a):
+    return (
+        (Y[firstPosition] - Y[midPosition])
+        / (Y[firstPosition] - Y[lastPosition])
+        * (atan(a / D[firstPosition]) - atan(a / D[lastPosition]))
+        - atan(a / D[firstPosition])
+        + atan(a / D[midPosition])
+    )
+
+
+initialSolution = root_scalar(initialGuessFunction, x0=100, bracket=[1, 500])
+
+
+def squaredErrorFunction(x):
+    a = x[0]
+    b = x[1]
+    c = x[2]
+    error = 0
+    for i in range(n):
+        error += (D[i] - (a / np.tan(b * Y[i] + c))) ** 2
+    return error
+
+
+aMin = initialSolution.root - 50
+aMax = initialSolution.root + 50
+a = initialSolution.root
+b = (atan(a / D[firstPosition]) - atan(a / D[midPosition])) / (
+    Y[firstPosition] - Y[midPosition]
+)
+c = atan(a / D[firstPosition]) - b * Y[firstPosition]
+
+finalSolution = [a, b, c]
+finalSolutionError = squaredErrorFunction([a, b, c])
+a = aMin
+while a < aMax:
+    x0 = np.array([a, b, c])
+    minimizeSolution = minimize(
+        squaredErrorFunction, x0, bounds=[(aMin, aMax), (-1, 0), (0.1, 2)]
+    )
+    if minimizeSolution.success:
+        if minimizeSolution.fun < finalSolutionError:
+            finalSolution = minimizeSolution.x
+            finalSolutionError = minimizeSolution.fun
+        if int(100 * a) % 50 == 0:
+            print("\n", round(a - aMin, 2), "/", round(aMax - aMin, 2))
+            print("error: ", round(minimizeSolution.fun, 6))
+            print("minimum:", round(finalSolutionError, 6))
+            print(minimizeSolution.x)
+    a += 0.01
+print("finalSolution:")
+print(finalSolution)
+finalTime = time()
+print("time: {}".format(finalTime - initialTime))
